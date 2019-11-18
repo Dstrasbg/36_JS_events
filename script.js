9c555e8474199439131e6f764ac3f6c09e283568
dstrasbg <argudo.daniel@gmail.com>
2019-11-18 21:10:08 +0100

    fonct 1

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-addEventListener("click", function(){ };
+// FONCTIONNALITE 1: affficher "click numéro X" en console quand l'utilisateur clique su footer
+
+
+let eventsOnFooter = () => 
+{
+    let pageFooter = document.getElementsByTagName("footer")[0];
+    let count =1;
+    let onFooterClick = () =>
+    {
+        console.log(`clic numéro ${count}`);
+        count ++;   
+    } 
+    pageFooter.addEventListener("click", onFooterClick);
+}
+
+
+
+//EX: addEventListener("click", function(){};
+
+
+let perform = () => {
+    eventsOnFooter();
+
+
+}
+
+perform();
